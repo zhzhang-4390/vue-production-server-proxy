7a2276b89d76c88565a7a2a4a593909dba25ba65
Zhang Zhihao <zhzhang.4390@gmail.com>
2020-03-24 11:01:24 +0800

    Build template

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+
+    <p
+      style="margin-top: 1rem"
+    >Test this page in production environment (Docker). If you can see my Github Page below, that means the Nginx proxy works :)</p>
+
+    <p>Don't forget to give my repo a star if you find this helpful :)</p>
+
+    <div v-html="myGithubPage" style="margin-top: 5rem"></div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+
+  props: {
+    msg: String
+  },
+
+  data() {
+    return {
+      myGithubPage: null
+    };
+  },
+
+  created() {
+    axios.get("/zhzhang-4390").then(res => (this.myGithubPage = res.data));
+  }
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  devServer: {
+    proxy: "https://github.com"
+  }
+};
